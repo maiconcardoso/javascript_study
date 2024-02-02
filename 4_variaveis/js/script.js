4d9c09dcdeca7ccf96a9e4288786bb6be82c1538
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-02 06:52:47 -0300

    Estudando conceito de variáveis

---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,27 @@
+var teste = 1;
+
+nome = "João";
+
+console.log(teste);
+
+teste = 'Maicon';
+
+console.log(teste);
+
+console.log(nome);
+
+// erros de sintaxe
+
+// var &nome = "primeiro nome";
+
+// var 5nome = 'segundo nome';
+
+let testadando = 1;
+const ola = 2;
+
+console.log(testadando);
+console.log(ola);
+
+var meuNome;
+meuNome = "Maicon";
+console.log(meuNome);
