eab0f562ed85ab139882dc26f1ce0e7050c16ec6
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-02 06:38:42 -0300

    Estudando algumas particularidades da linguagem

---
+++ 3_particularidades_js/js/script.js
@@ -0,0 +1,24 @@
+console.log("Primeiro");
+
+console.log("Útilmo");
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// este é um comentário
+
+/*
+    Este é comentário de multiplas linhas
+    linha 2
+    linha 3
+    linha 4
+*/
+
+/**
+ * Cometários de documentação
+ * Author: Maicon Cardoso
+ * Versão: 1.0
+ */
