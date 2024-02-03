2129cc87af4fb6d96c65fcb3b5ddfd5557c206c8
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-03 15:49:02 -0300

    Aula sobre o tipo string

---
+++ 6_Strings/js/script.js
@@ -0,0 +1,20 @@
+var nome = "Maicon";
+var sobrenome = "Cardoso"
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "3.14");
+var frase = "Número PI: ";
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write("Olá!");
+
