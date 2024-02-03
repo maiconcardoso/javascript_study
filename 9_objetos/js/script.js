549a503b03f29e1a32672d08b872a627861655a0
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-03 19:56:24 -0300

    Analisando objetos e entendendo que também são considerados tipos de dados

---
+++ 9_objetos/js/script.js
@@ -0,0 +1,22 @@
+var obj = {
+    nome: 'Maicon',
+    idade: 34,
+    profissao: "Programador",
+    estaTrabalhando: true
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome é: " + obj.nome);
+
+obj.nome = "Pedro";
+console.log(obj.nome);
+
+obj.graduacao = true;
+
+console.log(obj);
