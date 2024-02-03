90a8f96a2237dd3dd3a5c823565a105d82e038fb
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-03 15:34:44 -0300

    Estudando alguns conceitos do tipo Number

---
+++ 5_Number/js/script.js
@@ -0,0 +1,19 @@
+var numero = 5;
+
+console.log("Número: " + numero);
+console.log(typeof numero);
+
+var float = 5.32;
+
+console.log("Número:" + float);
+console.log(typeof float);
+
+var textoComNumero = '523';
+
+console.log("Texto" +textoComNumero);
+console.log(typeof textoComNumero);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
