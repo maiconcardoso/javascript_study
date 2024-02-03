715c95521bdc8bc8000128068c2967a6a5171275
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-03 20:02:39 -0300

    Aprendendo sobre arrays em javascript

---
+++ 10_arrays/js/script.js
@@ -0,0 +1,17 @@
+var arr = [5, "Maicon", true, {teste: 1, teste: 2}];
+console.log(arr);
+
+var arr2 = [2, 4, 5, 6, 7];
+console.log(arr2);
+
+console.log(arry[1]);
+
+arr[4] = 10;
+
+console.log(arr);
+
+arr[0] = "teste";
+
+console.log(arr);
+
+console.log(typeof arr);
