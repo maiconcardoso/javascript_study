d085d4809868e5a4063505f62412c82b86099075
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-04 17:28:40 -0300

    Estundano estruturas condicionais e comparadores

---
+++ 12_operador_comparacao/js/script.js
@@ -0,0 +1,23 @@
+var idade = 18;
+var possuiCarro = 1; // 0 = falso e 1 = verdadeiro
+
+if (idade >= 18) {
+    console.log("Usuário pode fazer a carteira de habilitação");
+}
+
+if (idade <= 17) {
+    console.log("O usuário não pode fazer a habilitação");
+}
+
+if (possuiCarro) {
+    console.log("O usuário já pode andar de carro");
+}
+
+var nome = "Maicon";
+if (nome == "Maicon") {
+    console.log("O seu nome é Maicon");
+}
+
+if (nome != "Maicon") {
+    console.log("O seu nome não é Maicon");
+}
