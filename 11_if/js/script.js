34e8c7c11412fed388bbd41ca2e68f8d38f1c889
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-04 17:11:46 -0300

    Estudando estrutura condicional if

---
+++ 11_if/js/script.js
@@ -0,0 +1,22 @@
+
+console.log("Antes do if");
+
+if (false) {
+    console.log("testando if");
+}
+
+console.log("Depois do if");
+
+if (true) {
+    console.log("Novo teste do if");
+}
+
+var idade = 19;
+var idadeMinina = 16;
+if (idade > idadeMinina) {
+    console.log("Pode fazer a carteira de habilitação");
+}
+ 
+if (idade < 15) {
+    console.log("Precisar esperar completar 18 anos");
+}
