6d942b9de56707777eef766d431c2aa6679c5be7
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-05 20:36:07 -0300

    Estudando operador de comparação de tipo e valor

---
+++ 14_comparacao_tipo_valor/js/script.js
@@ -0,0 +1,14 @@
+var numero = '5';
+
+if (numero == 5 && numero === 5) {
+    console.log("O número é 5");
+} else {
+    console.log("O Tipo não é compatível");
+}
+
+var outroNumero = 5;
+if (outroNumero == 5 && outroNumero === 5) {
+    console.log("O valor é 5");
+} else {
+    console.log("O tipo não é compatível");
+}
