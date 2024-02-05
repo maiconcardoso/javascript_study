cb19921504a696b5f931ef9be1fc98c5ca1f350a
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-05 20:59:11 -0300

    Estudando os conceitos do operador lógico OR '||'

---
+++ 16_operadores_logicos_or/js/script.js
@@ -0,0 +1,13 @@
+var idade = 16;
+var nome = "João";
+
+if (nome == "João" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima");
+} else {
+    console.log("Não pode entrar");
+}
+
+
+if (nome == "João" && 15 > 20 || 10 == 10) {
+    console.log("testatando");
+}
