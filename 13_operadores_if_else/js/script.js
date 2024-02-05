fdbb660c45f1f87b19938a09cedafbc9fcfe8644
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-05 06:33:57 -0300

    Estudandado o conceitos das estruturas condicionais if else e else if

---
+++ 13_operadores_if_else/js/script.js
@@ -0,0 +1,19 @@
+var nome = "Maicon";
+
+if (nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if (nome == "Maicon"){
+    console.log("O nome dele é Maicon");
+} else {
+    console.log("O nome dele é: " + nome)
+}
+
+
+var idade = 19;
+
+if (idade > 20) {
+    console.log("Ele pode entrar na festa")
+} else if (idade >= 18) {
+    console.log("Ele só pode entrar com autoriazação")
+}
+
