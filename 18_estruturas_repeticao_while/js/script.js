e2138ae136ebedd03cec49045af9620bfe2d48a5
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-06 21:00:18 -0300

    Estudando estrutura de repetição while

---
+++ 18_estruturas_repeticao_while/js/script.js
@@ -0,0 +1,21 @@
+var x = 0;
+
+while(x < 5) {
+    console.log("Testando repetição: " + x);
+    x = x + 1;
+}
+
+var arr = ['teste', 'testando', 'a', b];
+var y = 0;
+
+while (y <- 3) {
+    console.log(arr[y]);
+    y += 1;
+}
+
+var palavra = "Maicon";
+var i = 0;
+while(i <= 6) {
+    console.log(palavra[i]);
+    i++;
+}
