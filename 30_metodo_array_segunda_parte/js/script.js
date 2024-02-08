66933cb9b28d60b4753b3d21bc5379855980127a
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-08 20:25:35 -0300

    Manipulando arrays através de métodos

---
+++ 30_metodo_array_segunda_parte/js/script.js
@@ -0,0 +1,17 @@
+// splice
+var arr = [1,2,3,4,5];
+console.log(arr);
+arr.splice(2, 0, 999);
+console.log(arr);
+arr.splice(4, 1);
+console.log(arr);
+
+// indexOf
+console.log(arr.indexOf(5));
+
+// join
+var arr2 = ["o", "rato", "roeu", "a", "roupa", "do", "rei", "roma"];
+console.log(arr2.join(","));
+
+// reverse
+console.log(arr2.reverse());
