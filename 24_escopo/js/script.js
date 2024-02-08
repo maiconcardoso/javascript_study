f040d932d8e8f0d2ef5bc70337d2aab10255cdb8
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-07 21:12:46 -0300

    Análise inicial sobre escopo de variáveis

---
+++ 24_escopo/js/script.js
@@ -0,0 +1,16 @@
+var x = 1;
+
+var y = 3;
+
+function teste() {
+    var z = 2;
+    console.log(z)
+    console.log(x);
+}
+teste();
+
+function testando() {
+    var z = 5;
+    console.log(z);
+}
+testando();
