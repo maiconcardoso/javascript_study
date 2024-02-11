d43bd5c214a942a0b825ba5b46f1b1b7f015a2e3
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-11 11:34:29 -0300

    Aprendendo a trabalhar com querySelector

---
+++ 34_query_selector/js/script.js
@@ -0,0 +1,13 @@
+var itensClass = document.getElementsByClassName('item');
+console.log(itensClass);
+
+// querySelectorAll
+var itensQuery = document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+var itensQuery2 = document.querySelectorAll('#lista li');
+console.log(itensQuery2);
+
+// querySelector
+var lista = document.querySelector('#lista');
+console.log(lista);
