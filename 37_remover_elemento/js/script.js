0685a3fd54cfcf7d37f5f9e957998ce3a2750641
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-12 06:40:43 -0300

    Estudando funções que removem elementos html

---
+++ 37_remover_elemento/js/script.js
@@ -0,0 +1,10 @@
+// removendo elemento filho
+var container = document.querySelector('#container');
+var p = document.querySelector('#container p');
+
+container.removeChild(p);
+
+
+// remover o elemento
+var subtitle = document.querySelector('.subtitile');
+subtitle.remove();
