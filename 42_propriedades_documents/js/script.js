c4b9721410e1137ba7d09fde5275b7d3c7b2f670
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-13 20:15:56 -0300

    Estudando algumas propriedades do document

---
+++ 42_propriedades_documents/js/script.js
@@ -0,0 +1,15 @@
+// Propriedades Document
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links);
+
+document.links[0].textContent = "Twitter";
+
+console.log(document.URL);
+
+console.log(document.title);
+
+document.title = "Aula 42";
+
