7eddc74a7c67047dfd312e433387257d0be66a3c
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-02-13 16:50:15 -0300

    Trabalhando com alteração de css via JS

---
+++ 41_alterando_css/js/script.js
@@ -0,0 +1,15 @@
+
+// seleciona o elemento
+var title = document.querySelector("#title");
+
+// adiciona o estilo css
+title.style.color = "red";
+
+// backgroud-color
+title.style.backgroundColor = "yellow";
+
+// selecionar elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
